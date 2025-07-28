e04306de8baeca1d1b27c1a5e54d87eaa238c5c3
niasua <nsuarez2410@gmail.com>
2025-07-28 18:00:47 -0300

    creates models

---
+++ ShiftsLogger.API/Models/Shift.cs
@@ -0,0 +1,11 @@
+﻿namespace ShiftsLogger.API.Models;
+
+public class Shift
+{
+    public int Id { get; set; }
+    public DateTime Start { get; set; }
+    public DateTime End { get; set; }
+    public ShiftType Type { get; set; }
+    public int WorkerId { get; set; }
+    public Worker Worker { get; set; } = null!;
+}

c1db2b056f2e963de69038baabb9ab9e59d1c8ac
niasua <nsuarez2410@gmail.com>
2025-07-28 18:03:46 -0300

    Adds db context

---
+++ ShiftsLogger.API/Data/ShiftsLoggerContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShiftsLogger.API.Models;
+
+namespace ShiftsLogger.API.Data;
+
+public class ShiftsLoggerContext : DbContext
+{
+    public DbSet<Worker> Workers { get; set; }
+    public DbSet<Shift> Shifts { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=ShiftsLoggerDb;Trusted_Connection=True;");
+}

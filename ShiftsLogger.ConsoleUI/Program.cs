33fc076f3f3ec570cd715aaa3b261fd442ec6b62
niasua <nsuarez2410@gmail.com>
2025-07-29 21:54:51 -0300

    adds the API service

--- ShiftsLogger.ConsoleUI/Program.cs
+++ ShiftsLogger.ConsoleUI/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ShiftsLogger.ConsoleUI;
+
+ApiService apiService = new ApiService();
+
+var workers = await apiService.GetWorkersAsync();
+
+if (workers != null)
+{
+    Console.WriteLine("Trabajadores:");
+    foreach (var w in workers)
+    {
+        Console.WriteLine($"ID: {w.Id} - Nombre: {w.Name} - Trabajo: {w.Job}");
+    }
+}
+else
+{
+    Console.WriteLine("No se pudo obtener la lista de trabajadores.");
+}

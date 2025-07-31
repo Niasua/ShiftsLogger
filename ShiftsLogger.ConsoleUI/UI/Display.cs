22e1832b2ad73acafeb29bc0b0aede55f550fd6c
niasua <nsuarez2410@gmail.com>
2025-07-31 18:57:14 -0300

    Display class created

---
+++ ShiftsLogger.ConsoleUI/UI/Display.cs
@@ -0,0 +1,29 @@
+﻿using ShiftsLogger.ConsoleUI.Models;
+using Spectre.Console;
+
+namespace ShiftsLogger.ConsoleUI.UI;
+
+public static class Display
+{
+    public static void ShowWorkers(List<Worker> workers)
+    {
+        var table = new Table();
+        table.Border(TableBorder.Rounded);
+        table.AddColumn("[yellow]Id[/]");
+        table.AddColumn("[blue]Name[/]");
+        table.AddColumn("[green]Job[/]");
+
+        var apiService = new ApiService();
+
+        foreach (var worker in workers)
+        {
+            table.AddRow(
+                worker.Id.ToString(),
+                worker.Name.ToString(),
+                worker.Job.ToString()
+                );
+        }
+
+        AnsiConsole.Write(table);
+    }
+}
